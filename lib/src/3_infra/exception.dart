2655d2ac42ffb1616a43362b1a77b3fe736273c6
kazuki.endo <kazuki.endo@qrioinc.com>
2022-06-08 17:04:17 +0900

    before check

--- lib/src/3_infra/exception.dart
+++ lib/src/3_infra/exception.dart
@@ -1,8 +1,35 @@
-class FormConvertException implements Exception {
-  final String message;
+enum CheckedExceptionType { invalidText, invalidSerialNo }
 
-  FormConvertException(this.message);
+class CheckedException implements Exception {
+  final dynamic error;
+  final CheckedExceptionType type;
+  final dynamic stackTrace;
+
+  CheckedException({required this.error, required this.type, this.stackTrace});
 
   @override
-  String toString() => message;
+  String toString() => error.toString();
+}
+
+class InvalidAuthException implements Exception {
+  final dynamic error;
+  final dynamic stackTrace;
+
+  InvalidAuthException({required this.error, this.stackTrace});
+
+  @override
+  String toString() => error.toString();
+}
+
+enum RuntimeExceptionType { badResponse }
+
+class RuntimeException implements Exception {
+  final dynamic error;
+  final RuntimeExceptionType type;
+  final dynamic stackTrace;
+
+  RuntimeException({required this.error, required this.type, this.stackTrace});
+
+  @override
+  String toString() => error.toString();
 }
